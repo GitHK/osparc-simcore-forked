a2aec567c0abc9fe53e7cbcb9923a8fa77188d03
Andrei Neagu <neagu@itis.swiss>
2024-04-16 13:49:26 +0200

    refactor

--- services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/scheduler/_setup.py
+++ services/dynamic-scheduler/src/simcore_service_dynamic_scheduler/services/scheduler/_setup.py
@@ -1,27 +1,23 @@
 from fastapi import FastAPI
-from faststream.redis import RedisBroker
-from settings_library.redis import RedisDatabase, RedisSettings
+from settings_library.rabbit import RabbitSettings
+from settings_library.redis import RedisDatabase
 
-from ._base import router
+from ..redis import get_redis_client
+from ._base import Scheduler
 
 
 def setup_scheduler(app: FastAPI) -> None:
-    settings: RedisSettings = app.state.settings.DYNAMIC_SCHEDULER_REDIS
+    rabbit_settings: RabbitSettings = app.state.settings.DYNAMIC_SCHEDULER_RABBITMQ
 
     async def on_startup() -> None:
-        scheduling_dsn = settings.build_redis_dsn(RedisDatabase.SCHEDULING)
-
-        app.state.fast_stream_broker = broker = RedisBroker(scheduling_dsn)
-        broker.include_router(router)
-        await broker.start()
+        app.state.scheduler = scheduler = Scheduler(
+            rabbit_settings, get_redis_client(app, RedisDatabase.SCHEDULING)
+        )
+        await scheduler.start()
 
     async def on_shutdown() -> None:
-        broker: RedisBroker = app.state.fast_stream_broker
-        await broker.close()
+        scheduler: Scheduler = app.state.scheduler
+        await scheduler.stop()
 
     app.add_event_handler("startup", on_startup)
     app.add_event_handler("shutdown", on_shutdown)
-
-
-def get_broker(app: FastAPI) -> RedisBroker:
-    return app.state.fast_stream_broker
